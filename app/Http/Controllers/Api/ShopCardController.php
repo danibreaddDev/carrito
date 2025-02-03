393c9497db66a851688e492a334478e23ac9fc29
danibreadddev <danpammes@alu.edu.gva.es>
2025-02-03 18:59:11 +0100

    cambios

---
+++ app/Http/Controllers/Api/ShopCardController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\ShopCard;
+use Illuminate\Http\Request;
+
+class ShopCardController extends Controller
+{
+    public function __construct() //proteger la api con el token
+    {
+        $this->middleware("auth:api");
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $shopCard = ShopCard::all();
+        return response()->json(["carrito" => "$shopCard"]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ShopCard  $shopCard
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ShopCard $shopCard)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ShopCard  $shopCard
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ShopCard $shopCard)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ShopCard  $shopCard
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ShopCard $shopCard)
+    {
+        //
+    }
+}
